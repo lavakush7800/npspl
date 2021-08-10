a4d22930c4f30b484e6aac81b249fe1b960c93b0
lavakush7800 <lavakushgiri@gmail.com>
2021-08-10 20:12:01 +0530

    modify code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {UsersDataService} from './services/users-data.service'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'npspl';
+  title = 'www.lstechsoftware.com';
   users:any;
   constructor(private userData:UsersDataService){
     userData.users().subscribe((data)=>{
